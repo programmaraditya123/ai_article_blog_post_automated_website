5988441d89e5356668fb24f6ee08a0fdfc66d77a
programmaraditya123 <officialadi4255@gmail.com>
2025-09-09 23:58:26 +0530

    fix2 .env.local

--- frontend/lib/api/mongodb.ts
+++ frontend/lib/api/mongodb.ts
@@ -4,9 +4,7 @@ const uri = process.env.MONGODB_URI;
 let client;
 let clientPromise;
 
-if (!uri) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
+
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
