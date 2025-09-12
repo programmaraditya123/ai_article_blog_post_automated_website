ebc314f6ca4bc1176adc9f6bcad45c7dd724e773
programmaraditya123 <officialadi4255@gmail.com>
2025-09-13 00:51:09 +0530

    aditya commit 2

--- frontend/components/icons/index.tsx
+++ frontend/components/icons/index.tsx
@@ -143,30 +143,25 @@ const SearchIcon: React.FC<SvgIconProps> = ({ size = 24, ...props }) => (
 
 const CrossIcon: React.FC<SvgIconProps> = ({ size = 24, ...props }) => (
   <svg
-    width="64px"
-    height="64px"
+    width={size}
+    height={size}
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    <g
-      id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    ></g>
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
     <g id="SVGRepo_iconCarrier">
-      {" "}
       <path
         d="M20 20L4 4.00003M20 4L4.00002 20"
         stroke="#000000"
-        stroke-width="2"
-        stroke-linecap="round"
-      ></path>{" "}
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
     </g>
   </svg>
 );
-
 const Icons = {
   DatabaseIcon,
   ArrowRightIcon,
